00ec4c344077ca81b014a06edce41ce14e067bbb
Le Hoang Long <le.hoang.long@outlook.com>
2021-11-28 01:52:10 +0800

    Fix url config

--- src/config/Url.tsx
+++ src/config/Url.tsx
@@ -1,11 +1,13 @@
 let _BACKEND_URL = "http://kim_thi_backend_1/backend"
-if (process.env.CLOUD === 'GOOGLE') {
-    _BACKEND_URL = "https://kim-thi-backend-rfqj7mlw2q-as.a.run.app/backend"
-}
-export const HOST_URL = _BACKEND_URL;
-
 let _FILESERVER_URL = "http://localhost/backend" 
-if (process.env.CLOUD === 'GOOGLE') {
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    // dev code
+} else {
+    // production code
+    _BACKEND_URL = "https://kim-thi-backend-rfqj7mlw2q-as.a.run.app/backend"
     _FILESERVER_URL = 'https://kim-thi-backend-rfqj7mlw2q-as.a.run.app/backend'
 }
+
+export const HOST_URL = _BACKEND_URL;
 export const FILESERVER_URL = _FILESERVER_URL;
