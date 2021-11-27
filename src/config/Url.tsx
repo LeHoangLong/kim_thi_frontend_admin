6ee0db49c106fe5f00f4581bde9b3d153d247203
Le Hoang Long <le.hoang.long@outlook.com>
2021-11-28 00:30:53 +0800

    Added url

--- src/config/Url.tsx
+++ src/config/Url.tsx
@@ -1,2 +1,11 @@
-export const HOST_URL = "http://localhost/backend";
-export const FILESERVER_URL = "http://localhost/backend";
+let _BACKEND_URL = "http://kim_thi_backend_1/backend"
+if (process.env.CLOUD === 'GOOGLE') {
+    _BACKEND_URL = "https://kim-thi-backend-rfqj7mlw2q-as.a.run.app/backend"
+}
+export const HOST_URL = _BACKEND_URL;
+
+let _FILESERVER_URL = "http://localhost/backend" 
+if (process.env.CLOUD === 'GOOGLE') {
+    _FILESERVER_URL = 'https://kim-thi-backend-rfqj7mlw2q-as.a.run.app/backend'
+}
+export const FILESERVER_URL = _FILESERVER_URL;
